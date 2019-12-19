c10024cb90b19e07e1c5aeb007a35e119ef53883
Ubuntu <ubuntu@ip-172-31-80-170.ec2.internal>
2019-12-19 01:38:49 +0000

    matchy notes

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,20 +11,45 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//initilize a variable named animal to an empty object
+//add the property "species" to animal using dot notation with a value 
+//add the property "name" to animal using either notaion (dod is better) with a value
+//add the property "noises" to animal using bracket notation with a value of an empty array
+//print "animal object"
 
+var animal = {};
+animal.species = "bird";
+animal["name"] = 'Red';
+animal.noises = [];
+console.log(animal);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//initilize a variable named noises to an empty array
+//using bracket notation add a value to its first element noises[0]
+//using push add another sound to noise
+//using unshift add another sound to noise
+//using the length of the array[noises] add another sound to noise to the end of the array
 
-
+var noises = [];
+noises[0] = "caw";
+noises.push("Sraw");
+noises.unshift("plop");
+noises[noises.length] = "Flap";
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//using brackets add the new noises property to the animal array
+//using dot notation add another sound to noises 
 
-
+animal['noises'] = noises;
+animal.noises.push("scratch");
 
 /* *******************************************************************
  * Step 4 - Review
@@ -48,13 +73,66 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//initilize a variable named animals to an empty array
+//push animal into animals
+//test the look of the code using console.log()
 
 
+/* Step 6 - A Collection of Animals
+ 1. [ ] Create a variable named `animals` and assign it to an empty array.
+ 2. [ ] `push` our `animal` that we created to `animals`.
+ 3. [ ] `console.log` `animals`. What does it look like?
+ 4. [ ] Create a variable called `duck` and assign it to the data:
+  - `{ species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] }`
+ 5. [ ] `push` `duck` to `animals`
+ 6. [ ] `console.log` `animals`. What does it look like?
+ 7. [ ] Create two more animal objects each with a species, a name, and at least two sounds sounds and add each one of them to `animals.
+ 8. [ ] `console.log` `animals`, and, `console.log` the length of `animals`. Is everything looking right?
+
+*/
+var animals = [];
+animals.push(animal);
+console.log(animals);
+//initilize a variable named duck to { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] }
+//push duck to animals
+//test that it looks right using console.log()
+var duck = { species: 'duck',
+             name: 'Jerome',
+             noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+animals.push(duck);
+console.log(animals);
+//initilize a variable with a different dog to with an object with keys of species, name, & noises with at least 2 sounds on noises
+//push duck to animals
+//test that it looks right using console.log()
+var dog = { species: 'dog',
+             name: 'Mike',
+             noises: ['Woof!', 'grr', 'sneeze', 'snore'] };
+animals.push(dog);
+var cat = { species: 'cat',
+             name: 'Princess',
+             noises: ['Hiss!', 'scratch', 'purr', 'plop'] };
+animals.push(cat);
+console.log(animals);
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//arrays are a zero indexed list without the key value pairs of an object and we only need the name
+//this will be usefull later when using math.random() to select a random index in the array
+var friends = [];
+//use another function to generate a random number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+
+function getRandom(animals){
+    return animals[getRandomInt(animals.length -1)];
+}
+
+friends.push(getRandom(animals).name);
+getRandom(animals).friends = friends;
+console.log(animals);
 
 
 /**
--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,25 +12,65 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//create a function that takes to parameters, animals (an array) & name (a string)
+//if the animal(name) exists return the animal
+//if it does not exist return null
+function search(animals, name) {
+   for (var i = 0; i < animals.length; i++){
+       if (animals[i].name === name){
+           return animals[i];
+       }
+    }  return null;
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//create a function that searches through animals(an array)  a name(a string) to search for and its replacement( a string)
+//use a loop to iterate through animals and if a match is found replace it 
 
+function replace(animals, name, replacement){
+    for(let i = 0; i < animals.length; i++){
+        if(animals[i].name === name){
+            return animals.splice(animals[i], 1, replacement);
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//search through animals(array) for name(string)
+//if a match is found remove it
+//for loop to iterate through an array
+//use splice to remove the name if found
+function remove(animals, name){
+    for(let i = 0; i < animals.length; i++){
+        if(animals[i].name === name){
+            return animals.splice(animals[i]);
+        }
+        else{return ;}
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Create ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// make a function called `add` that has the parameters of animals & animal 
+//animals is an array and animal is the object
+//check tha the name property exists
+//check that the species property exists
+//checks that the names do NOT match
+//if these are all true push the animal object to the animals array
 
+function add(animals, animal) {
+   if(animal.species.length > 0 && animal.name.length > 0 && !animals.some((element) => (element.name === animal.name))) {
+           return animals.push(animal);
+           }
+        }
 
 
 /**
